8f038c578e8d41ba7502cf81247a23109ce77bc8
ALi <1142779811@qq.com>
2018-12-11 09:50:17 +0800

    这是第三次修改

--- practice/src/App.vue
+++ practice/src/App.vue
@@ -6,9 +6,27 @@
 
 <style lang="scss">
   // 公共样式
-  *{margin:0;padding:0;}
-  ol,ul,li{list-style:none;margin-bottom:0;}
-  h1,h2,h3,h4,h5,h6{font-weight:normal;margin:0;font-size:14px;}
-  img{border:none;vertical-align:middle;}
-  .clearfix:after{clear:both;content:"";display:block;height:0;}
+  * {
+  margin: 0;
+  padding: 0;
+  }
+  ol,ul,li {
+    list-style: none;
+    margin-bottom: 0;
+  }
+  h1,h2,h3,h4,h5,h6 {
+    font-weight: normal;
+    margin: 0;
+    font-size: 14px;
+  }
+  img {
+    border: none;
+    vertical-align: middle;
+  }
+  .clearfix:after {
+    clear: both;
+    content: "";
+    display: block;
+    height: 0;
+  }
 </style>
